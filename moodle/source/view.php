8a92a96afe8a6c1d9967d74b68e8fba46c98c270
NiklasStemberg <stemberg.n@gmail.com>
2019-06-19 04:07:45 +0200

    Demo Implementation of PDF Generation and Camunda Integration

---
+++ moodle/source/view.php
@@ -0,0 +1,87 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+/**
+ * Prints a particular instance of checkdeadline
+ *
+ * You can have a rather longer description of the file as well,
+ * if you like, and it can span multiple lines.
+ *
+ * @package    mod_ausleihverwaltung
+ * @copyright  2016 Your Name <your@email.address>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+// Replace checkdeadline with the name of your module and remove this line.
+require_once(dirname(dirname(dirname(__FILE__))).'/config.php');
+require_once(dirname(__FILE__).'/lib.php');
+require_once(dirname(__FILE__).'/locallib.php');
+require_once(dirname(__FILE__).'/resource_class.php');
+
+global $SESSION;
+
+$id = optional_param('id', 0, PARAM_INT); // Course_module ID, or
+$n  = optional_param('n', 0, PARAM_INT);  // ... checkdeadline instance ID - it should be named as the first character of the module.
+if ($id) {
+    $cm         = get_coursemodule_from_id('ausleihverwaltung', $id, 0, false, MUST_EXIST);
+    $course     = $DB->get_record('course', array('id' => $cm->course), '*', MUST_EXIST);
+    $ausleihverwaltung  = $DB->get_record('ausleihverwaltung', array('id' => $cm->instance), '*', MUST_EXIST);
+} else if ($n) {
+    $ausleihverwaltung  = $DB->get_record('ausleihverwaltung', array('id' => $n), '*', MUST_EXIST);
+    $course     = $DB->get_record('course', array('id' => $ausleihverwaltung->course), '*', MUST_EXIST);
+    $cm         = get_coursemodule_from_instance('ausleihverwaltung', $ausleihverwaltung->id, $course->id, false, MUST_EXIST);
+} else {
+    error('You must specify a course_module ID or an instance ID');
+}
+require_login($course, true, $cm);
+$event = \mod_ausleihverwaltung\event\course_module_viewed::create(array(
+    'objectid' => $PAGE->cm->instance,
+    'context' => $PAGE->context,
+));
+$event->add_record_snapshot('course', $PAGE->course);
+$event->add_record_snapshot($PAGE->cm->modname, $ausleihverwaltung);
+$event->trigger();
+/* PAGE belegen*/
+$PAGE->set_url('/mod/ausleihverwaltung/view.php', array('id' => $cm->id));
+$PAGE->set_title(format_string($ausleihverwaltung->name));
+$PAGE->set_heading(format_string($course->fullname));
+
+// Conditions to show the intro can change to look for own settings or whatever.
+if ($ausleihverwaltung->intro) {
+    echo $OUTPUT->box(format_module_intro('ausleihantrag', $ausleihverwaltung, $cm->id), 'generalbox mod_introbox', 'ausleihantragintro');
+}
+
+// Hier beginnt die Ausgabe
+echo $OUTPUT->header();
+$strName = "Login as student:";
+echo $OUTPUT->heading($strName);
+echo $OUTPUT->single_button(new moodle_url('../ausleihverwaltung/main_student_view.php', array('id' => $cm->id)), 'Login');
+echo '<br>';
+echo '<br>';
+
+$strName = "Login as DHBW-authority:";
+echo $OUTPUT->heading($strName);
+echo $OUTPUT->single_button(new moodle_url('../ausleihverwaltung/main_lab_view.php', array('id' => $cm->id)), 'Login');
+echo '<br>';
+echo '<br>';
+
+$strName = "Generate PDF";
+echo $OUTPUT->heading($strName);
+echo $OUTPUT->single_button(new moodle_url('../ausleihverwaltung/pdf_generate.php', array('id' => $cm->id)), 'Generate');
+echo '<br>';
+echo '<br>';
+
+
+// Finish the page.
+echo $OUTPUT->footer();
